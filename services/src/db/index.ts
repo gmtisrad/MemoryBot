5046cda930067089247ac1185deb73fbb2438979
Gabe Timm <Gabe.Timm@nike.com>
2023-06-06 21:33:45 -0700

    working

--- services/src/db/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getDb } from './mongoInit';
-
-export const dbRouter = express.Router();
-
-export const createEntry = async () => {
-  const mongoDB = await getDb();
-};
